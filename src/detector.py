5e6581424920dcb1118c53bd5f90b67f806a822a
BaburshahSayer <babursayer@gmail.com>
2025-05-18 22:20:22 +0100

    Add new changes.

--- src/detector.py
+++ src/detector.py
@@ -54,7 +54,7 @@ class ObjectDetector:
             'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
             'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
             'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-            'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant',
+            'orange', 'broccoli', 'carrot', 'hot dog', 'clearzza', 'donut', 'cake', 'chair', 'couch', 'potted plant',
             'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
             'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
             'scissors', 'teddy bear', 'hair drier', 'toothbrush'
